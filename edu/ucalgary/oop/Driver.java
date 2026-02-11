67c87c346342e0916d707893cc57de5699edd833
herschelFoster1 <herschel.foster1@ucalgary.ca>
2026-02-11 11:32:59 -0700

    Fix Driver

--- edu/ucalgary/oop/Driver.java
+++ edu/ucalgary/oop/Driver.java
@@ -1,5 +1,18 @@
-package edu.ucalgary.oop;
-
 public class Driver {
-    
+    String name;
+
+    public Driver(String name){
+        this.name = name;
+    }
+    public Driver(Driver d){
+        name = d.name;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String n){
+        name = n;
+    }
 }
+
